1799fe6c83774fcc06ab1687be05bdb0cd4825cb
karamba <kadykov.x@gmail.com>
2024-01-02 14:12:59 +0900

    step 1

---
+++ gendiff.js
@@ -0,0 +1,6 @@
+import {program}  from 'commander';
+
+program.description('Compares two configuration files and shows a difference.');
+program.version('ds');
+program.helpOption('-h, --help', 'output usage information');
+program.parse();
