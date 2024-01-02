8da310073544d2dd734d4d5b50ca160226175fae
karamba <kadykov.x@gmail.com>
2024-01-02 22:05:28 +0900

    step 2

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import {program}  from 'commander';
 
-program.description('Compares two configuration files and shows a difference.');
-program.version('ds');
-program.helpOption('-h, --help', 'output usage information');
-program.parse();
+program
+.version('1.0.0')
+.description('Compares two configuration files and shows a difference.')
+.helpOption('-h, --help', 'output usage information')
+.argument('<filepath1>')
+.argument('<filepath2>')
+.option('-f, --format <type>', 'output format')
+.parse();
