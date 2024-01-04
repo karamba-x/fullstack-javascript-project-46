1c278450d459c97801fab2cec7c96ddd86bc94b4
karamba <kadykov.x@gmail.com>
2024-01-04 21:33:13 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/karamba-x/fullstack-javascript-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/karamba-x/fullstack-javascript-project-46/actions)
+[![Actions Status](https://github.com/AfViktor/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/AfViktor/frontend-project-46/actions)
+
+[![Actions Status](https://github.com/AfViktor/frontend-project-46/actions/workflows/check.yml/badge.svg)](https://github.com/AfViktor/frontend-project-46/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/6cd37102cff86db6482c/maintainability)](https://codeclimate.com/github/AfViktor/frontend-project-46/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/6cd37102cff86db6482c/test_coverage)](https://codeclimate.com/github/AfViktor/frontend-project-46/test_coverage)
+
+## Описание
+Сравнивает 2 файла на наличие различий. Поддерживает форматы:
+- json
+- yml/yaml
+Результат сравнения можно отформатировать в:
+- JSON
+- plain
+- stylish
+
+## Минимальные системные требования
+node.js
+
+## Установка
+make install
+
+## Запуск
+gendiff -h - показать справку
+gendiff путь до файла №1 путь до файла №2
+
+Видео инструкция и пример работы программы со всеми форматами и структурами файлов:
+https://asciinema.org/a/b5180i74CyL72uqIdPtPTHw5q
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,58 @@
-import * as fs from "node:fs";
+import _ from 'lodash';
 import path from 'path';
+import fs from 'fs';
 
-export const readFile = (filepath) => {
-  const dirName = process.cwd(filepath);
-  const fullPath = path.resolve(dirName, filepath);
-  try {
-    return fs.readFileSync(fullPath, 'utf-8');
-  } catch (error) {
-    console.log(error);
-  }
+function readFile(filePath) {
+  const dirName = process.cwd(filePath);
+  const fullPath = path.resolve(dirName, filePath);
+  return fs.readFileSync(fullPath, 'utf-8');
 }
 
-export const getExtension = (filename) => {
+function getExtension(filename) {
   const result = filename.split('.');
   return result.at(-1);
 }
 
+function getDifferentObject(obj1, obj2) {
+  const allKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2))).map((key) => {
+    const oldValue = obj1[key];
+    const newValue = obj2[key];
+    if (!_.has(obj2, key)) {
+      return {
+        action: 'deleted',
+        key,
+        oldValue,
+      };
+    }
+    if (!_.has(obj1, key)) {
+      return {
+        action: 'added',
+        key,
+        newValue,
+      };
+    }
+    if (_.isObject(oldValue) && _.isObject(newValue)) {
+      return {
+        action: 'nested',
+        key,
+        children: getDifferentObject(oldValue, newValue),
+      };
+    }
+    if (oldValue !== newValue) {
+      return {
+        action: 'changed',
+        key,
+        oldValue,
+        newValue,
+      };
+    }
+    return {
+      action: 'unchanged',
+      key,
+      oldValue,
+    };
+  });
+  return allKeys;
+}
+
+export { readFile, getDifferentObject, getExtension };
